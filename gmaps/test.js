2bb9350eaec52c8efd6803fc2890612640885a88
wemjii <60511633+wemjii@users.noreply.github.com>
2021-01-27 16:35:16 -0500

    implementing a loop through array

--- gmaps/test.js
+++ gmaps/test.js
@@ -1,12 +1,26 @@
-let jsonArr = [
-  { Name: "Aaron", Latitude: "49.758123", Longitude: "-92.65349" },
-  { Name: "Algonquin", Latitude: "45.442046", Longitude: "-78.820583" },
+const locations = [
+  {
+    OID: 1,
+    name: "Old Faithful Lodge",
+    PopupInfo: "Cafeteria, Gift Shop, and Cabin Front Desk",
+    lat: 44.459415000000035,
+    lng: -110.82552899999996,
+  },
+  {
+    OID: 2,
+    name: "Old Faithful Inn",
+    PopupInfo:
+      "Largest log cabin structure.  Rooms, Dining, Bar, Gift Shop, Deck overlooking Old Faithful",
+    lat: 44.46012700000006,
+    lng: -110.83104399999996,
+  },
 ];
 
-let nameMod = jsonArr.map((obj) => {
-  return {
-    lat: obj.Latitude,
-    lng: obj.Longitude,
-  };
-});
-console.log(nameMod);
+// console.log(locations[0].name)
+console.log(locations.length);
+
+// Must be var because i changes
+var i = 0;
+for (i = 0; i < locations.length; i++) {
+  console.log(locations[i].name, locations[i].lat, locations[i].lat);
+}
